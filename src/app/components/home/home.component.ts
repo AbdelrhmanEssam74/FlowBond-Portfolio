9b711911a6853418afd93d5302ec138b49a92191
Abdelrhman <abdelrhmanroshdy8@gmail.com>
2025-06-27 21:28:01 +0300

    Integrate `ClientsComponent` into `HomeComponent` and update imports and template structure.

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import {HerosectionComponent} from '../herosection/herosection.component';
+import {ClientsComponent} from '../clients/clients.component';
 
 @Component({
   selector: 'app-home',
   imports: [
-    HerosectionComponent
+    HerosectionComponent,
+    ClientsComponent
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'

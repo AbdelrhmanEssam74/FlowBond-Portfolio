6225ad7de72a1524b9c20aa05f9915554f06a809
Abdelrhman <abdelrhmanroshdy8@gmail.com>
2025-07-07 00:00:15 +0300

    Integrate `ServicesComponent` into `HomeComponent` and update imports and template structure.

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -2,13 +2,15 @@ import { Component } from '@angular/core';
 import {HerosectionComponent} from '../herosection/herosection.component';
 import {ClientsComponent} from '../clients/clients.component';
 import {CareerJourneyComponent} from '../career-journey/career-journey.component';
+import {ServicesComponent} from '../services/services.component';
 
 @Component({
   selector: 'app-home',
   imports: [
     HerosectionComponent,
     ClientsComponent,
-    CareerJourneyComponent
+    CareerJourneyComponent,
+    ServicesComponent
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'

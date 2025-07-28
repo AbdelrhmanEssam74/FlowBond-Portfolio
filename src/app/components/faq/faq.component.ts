e51f98b3e30018d24357e9486f492fd6b1a4c02e
Abdelrhman <abdelrhmanroshdy8@gmail.com>
2025-07-28 16:15:58 +0300

    Refactor `FaqComponent` to use dynamic data binding, optimize responsive styles, and implement animation logic for toggling FAQ visibility.

--- src/app/components/faq/faq.component.ts
+++ src/app/components/faq/faq.component.ts
@@ -1,11 +1,53 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-faq',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './faq.component.html',
-  styleUrl: './faq.component.css'
+  styleUrls: ['./faq.component.css']
 })
 export class FaqComponent {
+  fqaData = [
+    {
+      question: 'What services do you offer?',
+      answer: 'When you first start reading, it takes a second to realize his wife\n' +
+        'has passed away. I thought this was so well done, &nbsp;because at first he’s talking to her all the time',
+      visible: false
+    },
+    {
+      question: ' How many revisions do I get?',
+      answer: 'When you first start reading, it takes a second to realize his wife\n' +
+        '          has passed away. I thought this was so well done, &nbsp;because at first he’s talking to her all the time',
+      visible: false
+    },
+    {
+      question: ' How much do your design services cost?',
+      answer: 'When you first start reading, it takes a second to realize his wife\n' +
+        '          has passed away. I thought this was so well done, &nbsp;because at first he’s talking to her all the time',
+      visible: false
+    },
+    {
+      question: 'How do I get started with a design project?',
+      answer: 'When you first start reading, it takes a second to realize his wife has passed away. I thought this was so well done,  because at first he’s talking to her all the time',
+      visible: false
+    },
+    {
+      question: 'How long does a design project take?',
+      answer: 'When you first start reading, it takes a second to realize his wife has passed away. I thought this was so well done,  because at first he’s talking to her all the time',
+      visible: false
+    },
+  ]
+
+  toggleFaq(index: number): void {
+    this.fqaData[index].visible = !this.fqaData[index].visible;
+    // Hide other FAQs when one is opened
+    this.fqaData.forEach((faq , i)=>{
+      if (i !== index) (
+        this.fqaData[i].visible = false
+      )
+    })
+  }
 
 }
